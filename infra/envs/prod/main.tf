0b028834c262a12635ef0d3c0aaab5487a61c79d
gawal shankar <gawalshankar@gmail.com>
2026-07-06 13:58:26 +0530

    Format Terraform files

--- infra/envs/prod/main.tf
+++ infra/envs/prod/main.tf
@@ -30,15 +30,15 @@ module "network" {
 }
 
 module "ecs" {
-  source              = "../../modules/ecs"
-  name_prefix         = local.name_prefix
-  aws_region          = var.aws_region
-  vpc_id              = module.network.vpc_id
-  public_subnet_ids   = module.network.public_subnet_ids
-  private_subnet_ids  = module.network.private_subnet_ids
-  container_image     = var.container_image
-  desired_count        = var.desired_count
-  tags                = local.tags
+  source             = "../../modules/ecs"
+  name_prefix        = local.name_prefix
+  aws_region         = var.aws_region
+  vpc_id             = module.network.vpc_id
+  public_subnet_ids  = module.network.public_subnet_ids
+  private_subnet_ids = module.network.private_subnet_ids
+  container_image    = var.container_image
+  desired_count      = var.desired_count
+  tags               = local.tags
 }
 
 module "rds" {
--- infra/envs/prod/terraform.tfvars
+++ infra/envs/prod/terraform.tfvars
@@ -1,8 +1,8 @@
-aws_region               = "us-east-1"
-vpc_cidr                 = "10.1.0.0/16"
-container_image          = "nginx:latest"
-desired_count            = 2
-db_instance_class        = "db.t3.small"
-backup_retention_period  = 7
-deletion_protection      = true
-multi_az                 = true
+aws_region              = "us-east-1"
+vpc_cidr                = "10.1.0.0/16"
+container_image         = "nginx:latest"
+desired_count           = 2
+db_instance_class       = "db.t3.small"
+backup_retention_period = 7
+deletion_protection     = true
+multi_az                = true
